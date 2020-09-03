eb9f94cfb050c39e60e8ca5167aadb4859087b64
godtide <ayoadebabatide@gmail.com>
2020-09-03 12:14:35 +0100

    vault

---
+++ vault/HackVault.js
@@ -0,0 +1,8 @@
+//await web3.eth.getStorageAt(address, position)
+const password = await web3.eth.getStorageAt(instanceof, 1);
+await contract.unlock(password);
+
+
+
+To learn: Private modifier doesn't change the visibility of a state variable, while a private variable is not visible calling from another contract, the variable is accessible from the index of the smart contract. 
+key takeaway: Don't use Private modifier to conceal information.
